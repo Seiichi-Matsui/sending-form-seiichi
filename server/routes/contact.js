84459e451289ecfee74d3b932d7545fcbce0c061
Seiichi-Matsui <s-matsui@zima-corp.com>
2022-11-02 13:14:29 +0900

    デプロイ用調整

--- server/routes/contact.js
+++ server/routes/contact.js
@@ -12,51 +12,51 @@ router.post('/add', function(req, res) {
     const { lastName, firstName, lastNamePseudonym, firstNamePseudonym, postCode, address, buildingName, phoneNumber, phoneType, emailFirst, emailLast, desiredContact, status, priority, manager, contactForm, date, opened } = req.body
 
     if(!lastName) {
-        return res.status(422).send({errors: [{title: 'User error', detail: "お名前を入力してください。"}]})
+        return res.status(422).send({errors: [{title: 'name1', detail: "お名前を入力してください。"}]})
     }
 
     if(!firstName) {
-        return res.status(422).send({errors: [{title: 'User error', detail: "お名前を入力してください。"}]})
+        return res.status(422).send({errors: [{title: 'name1', detail: "お名前を入力してください。"}]})
     }
 
     if(!firstNamePseudonym) {
-        return res.status(422).send({errors: [{title: 'User error', detail: "お名前のふりがなを入力してください。"}]})
+        return res.status(422).send({errors: [{title: 'name2', detail: "お名前のふりがなを入力してください。"}]})
     }
 
     if(!lastNamePseudonym) {
-        return res.status(422).send({errors: [{title: 'User error', detail: "お名前のふりがなを入力してください。"}]})
+        return res.status(422).send({errors: [{title: 'name2', detail: "お名前のふりがなを入力してください。"}]})
     }
 
     if(!postCode) {
-        return res.status(422).send({errors: [{title: 'User error', detail: "郵便番号を入力してください。"}]})
+        return res.status(422).send({errors: [{title: 'postCode', detail: "郵便番号を入力してください。"}]})
     }
 
     if(!address) {
-        return res.status(422).send({errors: [{title: 'User error', detail: "住所を入力してください。"}]})
+        return res.status(422).send({errors: [{title: 'adress', detail: "住所を入力してください。"}]})
     }
 
     if(!phoneNumber) {
-        return res.status(422).send({errors: [{title: 'User error', detail: "電話番号を入力してください。"}]})
+        return res.status(422).send({errors: [{title: 'phoneNumber', detail: "電話番号を入力してください。"}]})
     }
 
     if(!phoneType) {
-        return res.status(422).send({errors: [{title: 'User error', detail: "お電話の種類を選択してください。"}]})
+        return res.status(422).send({errors: [{title: 'phoneType', detail: "お電話の種類を選択してください。"}]})
     }
 
     if(!emailFirst) {
-        return res.status(422).send({errors: [{title: 'User error', detail: "メールアドレスを入力してください。"}]})
+        return res.status(422).send({errors: [{title: 'email', detail: "メールアドレスを入力してください。"}]})
     }
 
     if(!emailLast) {
-        return res.status(422).send({errors: [{title: 'User error', detail: "メールアドレスを入力してください。"}]})
+        return res.status(422).send({errors: [{title: 'email', detail: "メールアドレスを入力してください。"}]})
     }
 
     if(!desiredContact) {
-        return res.status(422).send({errors: [{title: 'User error', detail: "ご希望の連絡方法を選択してください。"}]})
+        return res.status(422).send({errors: [{title: 'desiredContact', detail: "ご希望の連絡方法を選択してください。"}]})
     }
 
     if(!contactForm) {
-        return res.status(422).send({errors: [{title: 'User error', detail: "お問合せ内容を入力してください。"}]})
+        return res.status(422).send({errors: [{title: 'contactForm', detail: "お問合せ内容を入力してください。"}]})
     }
 
     const contact = new Contact({lastName, firstName, lastNamePseudonym, firstNamePseudonym, postCode, address, buildingName, phoneNumber, phoneType, emailFirst, emailLast, desiredContact, status, priority, manager, contactForm, date, opened })
